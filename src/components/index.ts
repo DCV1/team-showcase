0c36a93bfc9be2799c2e054756f87a1e6d886562
milhaud <milhaud1201@gmail.com>
2024-02-29 00:53:00 +0900

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,8 @@
 
 export * from "./navbar";
 export * from "./footer";
-// export * from "./layout";
-export * from "./card";
+export * from "./layout";
+export * from "./book-card";
 export * from "./feature-card";
 export * from "./category-card";
 export * from "./fixed-plugin";
---
+++ src/components/layout.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ThemeProvider } from "@material-tailwind/react";
+
+export function Layout({ children }: { children: ReactNode }) {
+  return <ThemeProvider>{children}</ThemeProvider>;
+}
+
+export default Layout;
