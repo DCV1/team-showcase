d2987f10b156a450fd0bc4ee25566cc51b9aa294
milhaud <milhaud1201@gmail.com>
2024-02-29 00:48:24 +0900

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@
 
 export * from "./navbar";
 export * from "./footer";
-export * from "./layout";
+// export * from "./layout";
 export * from "./card";
 export * from "./feature-card";
 export * from "./category-card";
