c2efff30f1757322e0d54e04a08fa217a42f18c1
milhaud <milhaud1201@gmail.com>
2024-02-28 22:23:13 +0900

    fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
   return (
     <footer className="mt-10 px-8 pt-20">
       <div className="container mx-auto">
-        <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 gap-x-8 border-t border-blue-gray-50 py-6 md:justify-between" onResize={undefined} onResizeCapture={undefined}>
+        <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 gap-x-8 border-t border-blue-gray-50 py-6 md:justify-between">
           <Typography className="text-center font-normal !text-gray-700" placeholder='explanation' onResize={undefined} onResizeCapture={undefined}>
             &copy; {CURRENT_YEAR} Made with{" "}
             <a href="https://www.material-tailwind.com" target="_blank">
